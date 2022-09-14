5955d468d3d918fb206cb5b7eb4466d4d9f5e2ec
Tomer Harari <tharari21@gmail.com>
2022-09-14 16:19:08 -0400

    fighters array made

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-const fighterGrid = document.querySelector(".fighter-grid");
-const fighters = ["bryan", "anna", "bob", "kazuya"]
+const fighters = [
+  "heihachi",
+  "bryan",
+  "anna",
+  "feng",
+  "steve",
+  "hwoarang",
+  "jin",
+  "lars",
+  "paul",
+  "eddy",
+  "yoshimitsu",
+];
 const fighterUrl = (name) => {
     return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`;
 }
-const renderFighterCard = (name) => {
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.id = name;
-    const p = document.createElement('p')
-    p.textContent = name;
-    const img = document.createElement('img')
-    img.className= 'fighter-image'
-    img.src = fighterUrl(name)
-    card.append(img, p);
-    fighterGrid.append(card)
-
-}
-
-fighters.forEach(renderFighterCard)
