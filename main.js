48287a8e3c2ec9975f6a09ea1bef4d3036187fdf
Tomer Harari <tharari21@gmail.com>
2022-09-14 17:08:52 -0400

    Added navbar

--- main.js
+++ main.js
@@ -10,7 +10,25 @@ const fighters = [
   "paul",
   "eddy",
   "yoshimitsu",
+  "panda",
+  "ganryu",
+  "king",
+  "kuma",
+  "miguel",
 ];
 const fighterUrl = (name) => {
     return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`;
 }
+const fightersGrid = document.getElementById('fighters-grid');
+console.log("fighters grid", fightersGrid);
+
+fighters.forEach((fighter) => {
+    const card = document.createElement('div')
+    card.className = 'fighter-thumb' 
+    const img = document.createElement('img')
+    img.style.width = "100%";
+    img.style.height = '100%'
+    img.src = fighterUrl(fighter)
+    card.append(img)
+    fightersGrid.append(card)
+})
